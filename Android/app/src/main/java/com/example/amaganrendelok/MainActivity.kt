617e1e098c7dceca3f8d87f8a7ad503388585637
Suszter Dominik <suszterdominik@gmail.com>
2020-03-01 23:22:36 +0100

    Bejelentkező felület elkészítése

--- Android/app/src/main/java/com/example/amaganrendelok/MainActivity.kt
+++ Android/app/src/main/java/com/example/amaganrendelok/MainActivity.kt
@@ -4,12 +4,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        login_btn.setOnClickListener {
+            var status = if (username_et.text.toString().equals("test")
+                && passwd_et.text.toString().equals("test")
+            ) "Bejelentkezés sikeres" else "Bejelentkezés sikertelen"
+            Toast.makeText(this, status, Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -28,5 +37,4 @@ class MainActivity : AppCompatActivity() {
     private fun showInfo(){
 
     }
-
 }
