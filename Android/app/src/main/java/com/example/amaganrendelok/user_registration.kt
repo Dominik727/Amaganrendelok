255c4569c737e1369bab8a2c06581ced9b34af76
Dominik727 <45184845+Dominik727@users.noreply.github.com>
2020-03-02 23:24:38 +0100

    Regisztráció form
    - Regisztráció form létrehozása
    - a gomb még nem működik.

---
+++ Android/app/src/main/java/com/example/amaganrendelok/user_registration.kt
@@ -0,0 +1,13 @@
+package com.example.amaganrendelok
+
+import android.os.Bundle
+import android.view.View
+
+class user_registration {
+
+    fun onCreate(savedInstanceState: Bundle?) {
+
+
+    }
+
+}
