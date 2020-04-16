d320f311ca6dcc4bd42c4410f58ec5b13c546abc
Dominik727 <45184845+Dominik727@users.noreply.github.com>
2020-04-16 12:03:43 +0200

    Andoid Menü rendszer

--- Android/AMaganrendelok/app/src/main/java/AMaganrendelok/models/User.kt
+++
@@ -1,8 +0,0 @@
-package AMaganrendelok.models
-
-data class User(val id:Int,
-                val lastname:String,
-                val firstname:String,
-                val tel:String,
-                val email:String,
-                val TAJ:String)
---
+++ Android/AMaganrendelok/app/src/main/java/com/example/AMaganrendelok/activities/Surgeryes.kt
@@ -0,0 +1,16 @@
+package com.example.AMaganrendelok.activities
+
+import AMaganrendelok.api.Api
+import AMaganrendelok.api.RetrofitClient
+import androidx.appcompat.app.AppCompatActivity
+import com.example.AMaganrendelok.models.Surgery
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+class Surgeryes : AppCompatActivity() {
+
+
+
+}
